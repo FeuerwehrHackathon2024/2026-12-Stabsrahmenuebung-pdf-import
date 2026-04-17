2f067585128c38af2f3d8f7370856e89c966bbdc
Julius van Voorden <juliusvanvoorden@outlook.com>
2026-04-17 21:12:15 +0200

    basic auth

--- src/llm.ts
+++ src/llm.ts
@@ -4,12 +4,24 @@ const BASE_URL = Bun.env.OPENAI_API_BASE_URL || "http://localhost:11434/v1";
 const API_KEY = Bun.env.OPENAI_API_KEY || "do";
 const MODEL_NAME = Bun.env.OPENAI_MODEL_NAME || "llama3:8b-instruct-q4_K_M";
 
+const BASIC_AUTH_USER = Bun.env.OPENAI_USER || "user";
+const BASIC_AUTH_PASS = Bun.env.OPENAI_PASS || "pass";
+
 console.log("Using OpenAI API base URL:", BASE_URL);
 console.log("Using Model:", MODEL_NAME);
+
+function getBasicAuthHeader(user: string, pass: string): `Basic ${string}` {
+  const credentials = `${user}:${pass}`;
+  const encoded = btoa(credentials);
+  return `Basic ${encoded}`;
+}
 
 const client = new OpenAI({
   baseURL: BASE_URL,
   apiKey: API_KEY,
+  defaultHeaders: {
+    Authorization: getBasicAuthHeader(BASIC_AUTH_USER, BASIC_AUTH_PASS),
+  },
 });
 
 export async function completion(
